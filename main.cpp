d7b9406b6f37544554e31a1715c764fcc49434c5
unknown <montazer.as77@gmail.com>
2020-01-29 16:46:15 +0330

    add game page

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 using namespace sf;
 void aboutPage();
 void startPage();
+void gamePage();
 
 
 int main()
@@ -63,7 +64,7 @@ void aboutPage()
 
 void startPage()
 {
-	RenderWindow window(VideoMode(1280, 891), "Plumber Water Pipe Game", sf::Style::Titlebar | sf::Style::Close);
+	RenderWindow window(VideoMode(1280, 891), "Plumber Water Pipe Game", Style::Titlebar | Style::Close);
 	Texture IMG;
 	if (!IMG.loadFromFile("BGP.png"))
 	{
@@ -97,6 +98,14 @@ void startPage()
 
 				}
 			}
+			if (ev.mouseButton.button == Mouse::Left)
+			{
+				if ((ev.mouseButton.x >= 554 && ev.mouseButton.x <= 731) && (ev.mouseButton.y >= 250 && ev.mouseButton.y <= 428))
+				{
+					window.close();
+					gamePage();
+				}
+			}
 
 		}
 
@@ -105,5 +114,50 @@ void startPage()
 		window.display();
 
 	}
+
+}
+
+void gamePage()
+{
+	RenderWindow window(VideoMode(685,800 ), "Plumber Water Pipe Game(Game Page)", sf::Style::Titlebar | sf::Style::Close);
+
+	Texture IMGgamePage;
+	if (!IMGgamePage.loadFromFile("BG.png"))
+		return;
+	Sprite sp(IMGgamePage);
+	while (window.isOpen())
+	{
+		Event ev;
+		while (window.pollEvent(ev))
+		{
+
+			switch (ev.type)
+			{
+			case Event::Closed:
+				window.close();
+				break;
+			case Event::KeyPressed:
+				if (ev.key.code == Keyboard::Escape)
+					window.close();
+				break;
+			}
+
+			if (ev.mouseButton.button == Mouse::Left)
+			{
+				if ((ev.mouseButton.x >= 606 && ev.mouseButton.x <= 663) && (ev.mouseButton.y >= 14 && ev.mouseButton.y <= 57))
+				{
+					window.close();
+					startPage();
+
+				}
+			}
+
+		}
+
+		window.clear();
+		window.draw(sp);
+		window.display();
+	}
+
 
 }
